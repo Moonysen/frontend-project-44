103bbb60dcf458bf3b593166076e22e4d87062d9
Moonysen <noahwillsonn@gmail.com>
2023-03-24 04:15:03 +0400

    add game brain-even.js

---
+++ bin/brain-even.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+/* eslint-disable eol-last */
+/* eslint-disable linebreak-style */
+import startgame from '../src/braineven/startgame.js';
+
+startgame();
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,8 @@
 /* eslint-disable linebreak-style */
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!');
-const name = readlineSync.question('May I have your name? ');
-
-export default name;
+export default () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+};
